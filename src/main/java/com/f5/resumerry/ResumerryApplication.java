f4d1239c77988431b08b40d8702ba3d6953d3116
mrlee7 <>
2022-04-06 07:02:37 +0900

    init: first commit

---
+++ src/main/java/com/f5/resumerry/ResumerryApplication.java
@@ -0,0 +1,13 @@
+package com.f5.resumerry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumerryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResumerryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/f5/resumerry/ResumerryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f5.resumerry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumerryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
