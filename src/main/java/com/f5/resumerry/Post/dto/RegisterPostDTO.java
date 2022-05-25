32c38e1cf911d861c4785d4e37b2ef251d626181
qbinee <sdhg12@naver.com>
2022-05-25 19:49:42 +0900

    fix : 게시글 등록 수정

--- src/main/java/com/f5/resumerry/Post/dto/RegisterPostDTO.java
+++ src/main/java/com/f5/resumerry/Post/dto/RegisterPostDTO.java
@@ -3,9 +3,11 @@ package com.f5.resumerry.Post.dto;
 import com.f5.resumerry.selector.CategoryEnum;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @Data
+@NoArgsConstructor
 public class RegisterPostDTO {
     private String title;
     private CategoryEnum category;
